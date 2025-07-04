7d5c43a103129bd2488fb9613064ee264aade7ee
Valentyn Domanskyi <51060070+vdomanskyi@users.noreply.github.com>
2025-07-05 00:30:04 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1  style="width: 100%; text-align: center;">FREE K3R1NZZ</h1>
+
+  <img :src="img" alt="">
+</template>
+
+<script lang="ts" setup>
+import img from './assets/img.png'
+</script>
+
+
+<style lang="scss">
+body {
+display: flex;
+align-items: center;
+justify-content: center;
+
+#app {
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+
+height: 100vh;
+
+padding: 30px;
+
+img {
+width: 100%;
+object-fit: contain;
+max-height: 70vh;
+
+margin-top: 20px;
+}
+}
+}
+</style>
